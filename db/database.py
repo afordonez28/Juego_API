cbc13b9d2d24a7355c15ef8a48d43340cc7b7e10
Andres <afordonez28@ucatolica.edu.co>
2025-05-09 18:30:55 -0500

    creando el archivo .env y el de coneccion a la base de datos

--- db/database.py
+++ db/database.py
@@ -1,9 +1,27 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
 
-DATABASE_URL = "sqlite+aiosqlite:///./db/database.db"
+load_dotenv()
 
-engine = create_async_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+CLEVER_DB = (
+    f"postgresql+asyncpg://{os.getenv('CLEVER_USER')}:"
+    f"{os.getenv('CLEVER_PASSWORD')}@"
+    f"{os.getenv('CLEVER_HOST')}:"
+    f"{os.getenv('CLEVER_PORT')}/"
+    f"{os.getenv('CLEVER_DATABASE')}"
+)
 
-Base = declarative_base()
+engine: AsyncEngine = create_async_engine(CLEVER_DB, echo=True)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session():
+    async with async_session() as session:
+        yield session
